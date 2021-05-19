2bdadf8161914c4ba6f01f4e677f7ca529c338b0
ighosh <ighosh98>
2021-05-19 22:13:43 +0530

    CyActivator added

---
+++ src/main/java/gprofiler.internal/CyActivator.java
@@ -0,0 +1,26 @@
+package gprofiler.internal;
+
+import java.util.Properties;
+
+import org.cytoscape.app.swing.CySwingAppAdapter;
+import org.cytoscape.application.swing.CyAction;
+import org.cytoscape.service.util.AbstractCyActivator;
+import org.cytoscape.util.swing.OpenBrowser;
+import org.cytoscape.work.SynchronousTaskManager;
+import org.osgi.framework.BundleContext;
+
+public class CyActivator extends AbstractCyActivator {
+    public CyActivator() {
+        super();
+    }
+
+    public void start(BundleContext bc) {
+
+        CySwingAppAdapter adapter = getService(bc,CySwingAppAdapter.class);
+        OpenBrowser openBrowserService = getService(bc,OpenBrowser.class);
+        SynchronousTaskManager<?> syncTaskManager = getService(bc, SynchronousTaskManager.class);
+
+        BingoPluginAction bingoPluginAction = new BingoPluginAction(adapter, openBrowserService, syncTaskManager);
+        registerService(bc,bingoPluginAction, CyAction.class, new Properties());
+    }
+}
