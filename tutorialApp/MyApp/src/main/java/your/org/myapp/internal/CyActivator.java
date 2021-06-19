3ddfb9080894808b3be2ab929c211615e9da01e1
ighosh <ighosh98>
2021-06-19 15:44:45 +0530

    Visual design completed

--- tutorialApp/MyApp/src/main/java/your/org/myapp/internal/CyActivator.java
+++ tutorialApp/MyApp/src/main/java/your/org/myapp/internal/CyActivator.java
@@ -4,16 +4,23 @@ import org.cytoscape.application.CyApplicationManager;
 import org.cytoscape.model.CyNetworkFactory;
 import org.cytoscape.model.CyNetworkManager;
 import org.cytoscape.service.util.AbstractCyActivator;
+import org.cytoscape.view.model.CyNetworkView;
+import org.cytoscape.view.model.CyNetworkViewFactory;
+import org.cytoscape.view.model.CyNetworkViewManager;
 import org.cytoscape.work.ServiceProperties;
 import org.cytoscape.work.TaskFactory;
 import org.osgi.framework.BundleContext;
-import your.org.myapp.internal.tasks.MyAppTaskFactory;
+import org.cytoscape.view.vizmap.VisualMappingFunctionFactory;
+import org.cytoscape.view.vizmap.VisualMappingManager;
+import org.cytoscape.view.vizmap.VisualStyleFactory;
 
 import java.util.Properties;
 
+import your.org.myapp.internal.tasks.MyAppTaskFactory;
+
 /**
  * {@code CyActivator} is a class that is a starting point for OSGi bundles.
- * 
+ *
  * A quick overview of OSGi: The common currency of OSGi is the <i>service</i>.
  * A service is merely a Java interface, along with objects that implement the
  * interface. OSGi establishes a system of <i>bundles</i>. Most bundles import
@@ -21,12 +28,12 @@ import java.util.Properties;
  * service, it provides an implementation to the service's interface. Bundles
  * import a service by asking OSGi for an implementation. The implementation is
  * provided by some other bundle.
- * 
+ *
  * When OSGi starts your bundle, it will invoke {@CyActivator}'s
  * {@code start} method. So, the {@code start} method is where
  * you put in all your code that sets up your app. This is where you import and
  * export services.
- * 
+ *
  * Your bundle's {@code Bundle-Activator} manifest entry has a fully-qualified
  * path to this class. It's not necessary to inherit from
  * {@code AbstractCyActivator}. However, we provide this class as a convenience
@@ -44,12 +51,19 @@ public class CyActivator extends AbstractCyActivator {
 	 */
 	@Override
 	public void start(BundleContext context) throws Exception {
+		CyApplicationManager applicationManager = getService(context, CyApplicationManager.class);
 		CyNetworkFactory networkFactory = getService(context,CyNetworkFactory.class);
 		CyNetworkManager networkManager = getService(context,CyNetworkManager.class);
+		CyNetworkViewFactory viewFactory = getService(context,CyNetworkViewFactory.class);
+		CyNetworkViewManager viewManager = getService(context,CyNetworkViewManager.class);
+		VisualMappingManager vmm = getService(context,VisualMappingManager.class);
+		VisualStyleFactory visualStyleFactory = getService(context,VisualStyleFactory.class);
+		//continuous mapping strategy -> Refer slides
+		VisualMappingFunctionFactory continuousMappingFactory = getService(context,VisualMappingFunctionFactory.class,"(mapping.type=continuous)");
 		Properties props = new Properties();
 		props.put(ServiceProperties.PREFERRED_MENU,"Apps");
 		props.put(ServiceProperties.TITLE,"Hello World!");
-		TaskFactory factory = new MyAppTaskFactory(networkManager,networkFactory);
+		TaskFactory factory = new MyAppTaskFactory(networkManager,networkFactory,viewManager,viewFactory,vmm,visualStyleFactory,continuousMappingFactory);
 
 		//registers service with osgi
 		registerService(context,factory,TaskFactory.class,props);
--- tutorialApp/MyApp/src/main/java/your/org/myapp/internal/tasks/MyAppTaskFactory.java
+++ tutorialApp/MyApp/src/main/java/your/org/myapp/internal/tasks/MyAppTaskFactory.java
@@ -2,24 +2,39 @@ package your.org.myapp.internal.tasks;
 
 import org.cytoscape.model.CyNetworkFactory;
 import org.cytoscape.model.CyNetworkManager;
+import org.cytoscape.view.model.CyNetworkViewFactory;
+import org.cytoscape.view.model.CyNetworkViewManager;
+import org.cytoscape.view.vizmap.VisualMappingFunctionFactory;
+import org.cytoscape.view.vizmap.VisualMappingManager;
+import org.cytoscape.view.vizmap.VisualStyleFactory;
 import org.cytoscape.work.AbstractTaskFactory;
 import org.cytoscape.work.TaskIterator;
 
 public class MyAppTaskFactory extends AbstractTaskFactory {
-    final CyNetworkManager networkManager;
-    final CyNetworkFactory networkFactory;
-    public MyAppTaskFactory(CyNetworkManager networkManager, CyNetworkFactory networkFactory) {
+    final CyNetworkManager netManager;
+    final CyNetworkFactory netFactory;
+    final CyNetworkViewManager viewManager;
+    final CyNetworkViewFactory viewFactory;
+    final	VisualMappingManager vmm;
+    final	VisualMappingFunctionFactory cFactory;
+    final	VisualStyleFactory vFactory;
+    public MyAppTaskFactory(CyNetworkManager networkManager, CyNetworkFactory networkFactory,
+                            CyNetworkViewManager viewManager, CyNetworkViewFactory viewFactory,
+                            VisualMappingManager vmm, VisualStyleFactory vFactory,
+                            VisualMappingFunctionFactory cFactory) {
         super();
-        this.networkFactory = networkFactory;
-        this.networkManager = networkManager;
-    }
-    @Override
-    public TaskIterator createTaskIterator() {
-        return new TaskIterator( new myAppTask(networkManager,networkFactory));
+        netManager = networkManager;
+        netFactory = networkFactory;
+        this.viewManager = viewManager;
+        this.viewFactory = viewFactory;
+        this.vmm = vmm;
+        this.cFactory = cFactory;
+        this.vFactory = vFactory;
     }
 
-    @Override
-    public boolean isReady() {
-        return true;
+    public TaskIterator createTaskIterator () {
+        return new TaskIterator(new MyAppTask(netManager, netFactory, viewManager, viewFactory, vmm, vFactory, cFactory));
     }
+
+    public boolean isReady() { return true; }
 }
